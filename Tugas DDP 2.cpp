db7e031cc47232083621238fea98498ba56479a6
Muhammad-Faqqihna-Addien <2500018034@webmail.uad.ac.id>
2025-11-05 10:47:49 +0700

    tugas2_ddp.cpp

--- Tugas DDP 2.cpp
+++ Tugas DDP 2.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main() {
     long totdetik;
-    float jam, menit, detik;
+    float hari, jam, menit, detik;
 
     cout << "Masukkan Waktu (detik): ";
     cin >> totdetik;
 
-	jam   = (totdetik / 3600) % 24;
+    hari  =totdetik / 86400;
+    jam   = (totdetik / 3600) % 24;
     menit = ((totdetik / 60) % 60);
     detik = totdetik % 60;
 
     cout << endl;
     cout << totdetik << " detik = "
+         << hari << " hari "
          << jam << " jam "
          << menit << " menit "
          << detik << " detik." << endl;
