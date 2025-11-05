da5a1bcb0ed9f6f78842e59af2d9b8dc2bb01c14
Muhammad-Faqqihna-Addien <2500018034@webmail.uad.ac.id>
2025-11-05 10:42:08 +0700

    tugas DDP 2.cpp

--- Tugas DDP 2.cpp
+++ Tugas DDP 2.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	long detik,totaldetik;
-	int jam,menit,hari;
-	
-	cout<<"Masukkan Angka >1 (dalam satuan detik) : ";
-	cin>>detik;
-	
-	hari=detik/86400;
-	jam=(detik%86400)/3400;
-	menit=(detik%3600)/60;
-	totaldetik=(detik%60);
-	
-	cout<<endl;
-	cout<<"hasil konversi :";
-	cout<<detik<<" "<<"Detik"<<" "<<"="<<" "<<hari<<" "<<"Hari"<<" "<<jam<<" "<<"Jam"<<" "<<menit<<" "<<"Menit"<<" "<<totaldetik<<" "<<"Detik"<<endl;
-	
-	return 0;
+int main() {
+    long totdetik;
+    float jam, menit, detik;
+
+    cout << "Masukkan Waktu (detik): ";
+    cin >> totdetik;
+
+	jam   = (totdetik / 3600) % 24;
+    menit = ((totdetik / 60) % 60);
+    detik = totdetik % 60;
+
+    cout << endl;
+    cout << totdetik << " detik = "
+         << jam << " jam "
+         << menit << " menit "
+         << detik << " detik." << endl;
+
+    return 0;
 }
